a8d419761578f96d91d212852f30cbc4e6b22823
Enrique Castellanos <enrique.castellanos@qwizdom.com>
2018-10-24 17:10:17 +0100

    first release

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+
+import { store } from './store/store';
 
 Vue.config.productionTip = false
 
